3545f39ca4c1f48201e73cef733309886cafb0c8
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2022-08-03 23:10:43 +0700

    Update source and file
    commit -m "Update source and file"

--- bot/src/encode-filename.js
+++ bot/src/encode-filename.js
@@ -2,7 +2,7 @@
 function ConvertStringToHex(str) {
     var arr = [];
     for (var i = 0; i < str.length; i++) {//string to hex code
-           arr[i] = ("00" + str.charCodeAt(i).toString(16)).slice(-4);
+       arr[i] = ("00" + str.charCodeAt(i).toString(16)).slice(-4);
     }
     console.log("3ndc0d3c17" + arr.join("3ndc0d3c17"));
     return "3ndc0d3c17" + arr.join("3ndc0d3c17");
@@ -12,3 +12,12 @@ function ConvertStringToHex(str) {
   ConvertStringToHex(str);//Output: 3ndc0d3c1700643ndc0d3c1700613ndc0d3c1700743ndc0d3c170061
   let  con = "stirng";
   
+
+
+
+
+
+
+
+
+
